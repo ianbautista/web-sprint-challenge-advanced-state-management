a4ea75c982a6e795f51af6f427d147d0c3ae90c1
ianbautista <ianfbautista@gmail.com>
2020-08-14 08:22:52 -0700

    built action creators

---
+++ smurfs/src/store/actions/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_ERROR = "FETCH_SMURFS_ERROR";
+
+export const fetchSmurfs = () => {
+	return (dispatch) => {
+		dispatch({ type: FETCH_SMURFS_START });
+		axios
+			.get("http://localhost:3333/smurfs")
+			.then((response) => {
+				console.log(response);
+				dispatch({ type: FETCH_SMURFS_SUCCESS, payload: response.data });
+			})
+			.catch((error) => {
+				dispatch({ type: FETCH_SMURFS_ERROR, payload: error.message });
+			});
+	};
+};
